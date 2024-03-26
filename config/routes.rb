26f7decf41c36f6fa92fceddbbac28c9a505e3e8
Jeanne Keanu Echevarria <keanukho66@gmail.com>
2024-03-26 16:27:48 +0800

    still doing the active class

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'welcome#index'
+  get 'about', to: 'welcome#about'
+  get 'contact', to: 'welcome#contact'
 end
